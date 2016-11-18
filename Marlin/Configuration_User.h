7a6a0465f02da88210b70d37d0fa78aab447506f
CONSULitAS <hi345gr@MacBook-Air-JG.local>
2016-11-18 21:08:23 +0100

    Configuration_User.h: Cleanup and better documentation
    first serious public version

--- Marlin/Configuration_User.h
+++ Marlin/Configuration_User.h
@@ -1,13 +1,18 @@
-// Jochens Spezial-Config
+/**
+ * Sample configuration file for Vellemann K8200
+ *
+ * Please configure your individual hardware options for the K8200:
+ */
 
 #ifndef CONFIGURATION_USER_H
 #define CONFIGURATION_USER_H
 
+// DRV8825 stepper drivers
 #undef DEFAULT_AXIS_STEPS_PER_UNIT
 //#define DEFAULT_AXIS_STEPS_PER_UNIT   {64.25,64.25,2560, 600}  // default steps per unit for K8200
   #define DEFAULT_AXIS_STEPS_PER_UNIT   {128.5,128.5,2560,1370}  // default steps per unit for K8200 -> X,Y,E mit DRV8825
 
-// PID für E3D V6 mit Lüfter
+// PID for E3D V6 with fan
 #undef  DEFAULT_Kp
 #undef  DEFAULT_Ki
 #undef  DEFAULT_Kd
@@ -15,7 +20,7 @@
 #define  DEFAULT_Ki 1.58
 #define  DEFAULT_Kd 93.51
 
-// optimierter Bauraum
+// optimized build room
 #undef X_MAX_POS
 #undef Y_MAX_POS
 #undef Z_MAX_POS
@@ -23,16 +28,9 @@
 #define Y_MAX_POS 205
 #define Z_MAX_POS 205
 
+// german UI
 #undef LCD_LANGUAGE
 #define LCD_LANGUAGE de
-
-// TEMP: Extruder Fan direkt angeschlossen
-//#undef  EXTRUDER_0_AUTO_FAN_PIN
-//#undef  EXTRUDER_AUTO_FAN_SPEED
-//#undef  EXTRUDER_AUTO_FAN_TEMPERATURE
-//#define EXTRUDER_0_AUTO_FAN_PIN 8
-//#define EXTRUDER_AUTO_FAN_TEMPERATURE 100 // should be 50 -> hack for problem with thermistor
-//#define EXTRUDER_AUTO_FAN_SPEED   255  // default 255 == full speed
 
 #endif //CONFIGURATION_USER_H
 
